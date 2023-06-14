53f8de01572314a1087a3da8ec223985006a1752
GiorunoGiovanna <971615621@qq.com>
2023-06-14 14:54:28 +0800

    修改base url

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,6 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  // 设置base url防止打包后引入路径出错
+  base: 'https://giorunogiovanna.github.io/test/'
 })
